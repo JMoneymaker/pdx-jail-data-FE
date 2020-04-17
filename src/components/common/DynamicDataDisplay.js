db38891434e7966eca977d6b62e278641f7207a3
AaronMullan <aaroncmullan@gmail.com>
2020-04-17 07:32:29 -0700

    added plus sign to positive numbers

--- src/components/common/DynamicDataDisplay.js
+++ src/components/common/DynamicDataDisplay.js
@@ -3,21 +3,13 @@ import PropTypes from 'prop-types';
 
 
 const DynamicDataDisplay = ({ children, hook }) => {
+  const printableNumber = (n) =>  {return (n > 0) ? '+' + n : n;};
   const { data } = hook();
-  if(data === 0) return (
-
-    <div>
-      <h3>{children}</h3>
-      <div>
-        <p>Loading</p>
-      </div>
-    </div>
-  );
   return (
     <div>
       <h3>{children}</h3>
       <div>
-        <h2>{data}</h2>
+        <h2>{printableNumber(data)}</h2>
       </div>
     </div>
   );
