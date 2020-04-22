de247c29f2bdbb9d4a6386317f71f65f6e207e88
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-04-22 15:14:13 -0700

    responivene with basic breakpoints

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,38 +9,40 @@ const Home = () => {
 
   return (
     <main className={styles.Home}>
-      <section id={styles.mult} className={styles.dynamicWrapper}>
-        <div className={styles.countyTitle}>
-          <h3>Multnomah</h3>
-        </div>
-        <div className={styles.dynamicDisplay}>
-          <DynamicDataDisplay hook={useDailyTotal}>Currently in detention</DynamicDataDisplay>
-        </div>
-        <div className={styles.dynamicDisplay}>
-          <DynamicDataDisplay hook={useDailyChange}>Net one day change</DynamicDataDisplay>
-        </div>
-      </section>
-      <section id={styles.clack} className={styles.dynamicWrapper}>
-        <div className={styles.countyTitle}>
-          <h3>Clackamas</h3>
-        </div>
-        <div className={styles.dynamicDisplay}>
-          <DynamicDataDisplay hook={useDailyTotal}>Currently in detention</DynamicDataDisplay>
-        </div>
-        <div className={styles.dynamicDisplay}>
-          <DynamicDataDisplay hook={useDailyChange}>Net one day change</DynamicDataDisplay>
-        </div>
-      </section>
-      <section id={styles.wash} className={styles.dynamicWrapper}>
-        <div className={styles.countyTitle}>
-          <h3>Washington</h3>
-        </div>
-        <div className={styles.dynamicDisplay}>
-          <DynamicDataDisplay hook={useDailyTotal}>Currently in detention</DynamicDataDisplay>
-        </div>
-        <div className={styles.dynamicDisplay}>
-          <DynamicDataDisplay hook={useDailyChange}>Net one day change</DynamicDataDisplay>
-        </div>
+      <section className={styles.dynamo}>
+        <section id={styles.mult} className={styles.dynamicWrapper}>
+          <div className={styles.countyTitle}>
+            <h3>Multnomah</h3>
+          </div>
+          <div className={styles.dynamicDisplay}>
+            <DynamicDataDisplay hook={useDailyTotal}>Currently in detention</DynamicDataDisplay>
+          </div>
+          <div className={styles.dynamicDisplay}>
+            <DynamicDataDisplay hook={useDailyChange}>Net one day change</DynamicDataDisplay>
+          </div>
+        </section>
+        <section id={styles.clack} className={styles.dynamicWrapper}>
+          <div className={styles.countyTitle}>
+            <h3>Clackamas</h3>
+          </div>
+          <div className={styles.dynamicDisplay}>
+            <DynamicDataDisplay hook={useDailyTotal}>Currently in detention</DynamicDataDisplay>
+          </div>
+          <div className={styles.dynamicDisplay}>
+            <DynamicDataDisplay hook={useDailyChange}>Net one day change</DynamicDataDisplay>
+          </div>
+        </section>
+        <section id={styles.wash} className={styles.dynamicWrapper}>
+          <div className={styles.countyTitle}>
+            <h3>Washington</h3>
+          </div>
+          <div className={styles.dynamicDisplay}>
+            <DynamicDataDisplay hook={useDailyTotal}>Currently in detention</DynamicDataDisplay>
+          </div>
+          <div className={styles.dynamicDisplay}>
+            <DynamicDataDisplay hook={useDailyChange}>Net one day change</DynamicDataDisplay>
+          </div>
+        </section>
       </section>
       <section className={styles.chartsWrapper}>
         <DailyDetentionCounts />
