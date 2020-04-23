5976037547b7d9fd7c81f2692276038db157470e
AaronMullan <aaroncmullan@gmail.com>
2020-04-22 21:37:06 -0700

    font idea

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './common/Header';
 import DailyDetentionCounts from './charts/DailyCount';
+import './App.css';
 
 export default function App() {
   return (
