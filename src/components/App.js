b33937c0ef6413fe263c869d891cab8f92294a22
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-05-01 18:49:29 -0700

    first chart roughtly in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Landing from './landing/Landing';
-import styles from './App.css';
-import DailyRaceMult from './charts/DailyRaceMult';
+import DailyRaceMult from './charts/dailyRaceMult';
 
 export default function App() {
   return (
     <>
       <Landing />
-      <div className={styles.chartWrapper}>
-        <DailyRaceMult />
-      </div>
-
+      <DailyRaceMult/>
     </>
   );
 }
--- src/components/charts/DailyRaceMult.js
+++ src/components/charts/DailyRaceMult.js
@@ -1,14 +1,7 @@
 import { ResponsiveBar } from '@nivo/bar';
 import { getDailyRaceCount } from '../../services/getMultnomahDaily';
 import React, { useState, useEffect } from 'react';
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
-  
-
-
+import styles from './Charts.css';
 
 const DailyRaceMult = () => {
 
@@ -19,7 +12,17 @@ const DailyRaceMult = () => {
       .then(res => {setData(res);});
   }, []);
 
-  const mappedData = data.map(item => {
+  const alphabetical = data.sort(function(a, b){
+    if(a._id < b._id) { return -1; }
+    if(a._id > b._id) { return 1; }
+    return 0;
+  });
+  const complete = alphabetical.map(item => {
+    if(item._id === 'P'){
+      item._id = 'Pacific Islander';
+    } return item;
+  });
+  const mappedData = complete.map(item => {
     return ({
       id: item._id,
       total: item.total
@@ -27,64 +30,64 @@ const DailyRaceMult = () => {
   });
   
   return (
-    <ResponsiveBar
-      data={mappedData}
-      keys={['id', 'total']}
-      indexBy="id"
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-      padding={0.3}
-      colors={{ scheme: 'nivo' }}
-      fill={[]}
-      borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'race',
-        legendPosition: 'middle',
-        legendOffset: 32
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'number of people in jail today',
-        legendPosition: 'middle',
-        legendOffset: -40
-      }}
-      labelSkipWidth={12}
-      labelSkipHeight={12}
-      labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      legends={[
-        {
-          dataFrom: 'keys',
-          anchor: 'bottom-right',
-          direction: 'column',
-          justify: false,
-          translateX: 120,
-          translateY: 0,
-          itemsSpacing: 2,
-          itemWidth: 100,
-          itemHeight: 20,
-          itemDirection: 'left-to-right',
-          itemOpacity: 0.85,
-          symbolSize: 20,
-          effects: [
+    <div className={styles.chartPageContainer}>
+      <h3>Number of People in Custordy by Race</h3>
+      <div className={styles.chartWrapper}>
+        <ResponsiveBar
+          data={mappedData}
+          keys={['id', 'total']}
+          indexBy="id"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.4}
+          colors={'black'}
+          borderColor={'white'}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'race',
+            legendPosition: 'middle',
+            legendOffset: 40
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'number of people in jail today',
+            legendPosition: 'middle',
+            legendOffset: -40
+          }}
+          labelTextColor={'white'}
+          legends={[
             {
-              on: 'hover',
-              style: {
-                itemOpacity: 1
-              }
+              dataFrom: 'keys',
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemOpacity: 1
+                  }
+                }
+              ]
             }
-          ]
-        }
-      ]}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-    />
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+        />
+      </div>
+    </div>
   );
 };
 
