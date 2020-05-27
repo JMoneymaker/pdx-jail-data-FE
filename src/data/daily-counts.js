e2d1955de6e8009189a5d3c67a80a69a7324a249
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-05-27 08:59:14 -0700

    add dummy data

---
+++ src/data/daily-counts.js
@@ -0,0 +1,206 @@
+export const dailyCounts = { 
+  counts: [
+    {
+      _id: '5ea97b738f763200047bf3a9',
+      date: '2020-04-29T13:00:20.620Z',
+      clack: 137,
+      mult: 724,
+      wash: 476
+    },
+    {
+      _id: '5eaaccf35296c300047c27c3',
+      date: '2020-04-30T13:00:30.171Z',
+      clack: 133,
+      mult: 729,
+      wash: 475
+    },
+    {
+      _id: '5eac1fb5b699250004ce4a30',
+      date: '2020-05-01T13:01:08.066Z',
+      clack: 137,
+      mult: 721,
+      wash: 471
+    },
+    {
+      _id: '5ead702a3028d9000422ede0',
+      date: '2020-05-02T13:01:05.463Z',
+      clack: 142,
+      mult: 716,
+      wash: 465
+    },
+    {
+      _id: '5eaec19986207900043ddd0e',
+      date: '2020-05-03T13:00:48.702Z',
+      clack: 145,
+      mult: 743,
+      wash: 471
+    },
+    {
+      _id: '5eb0132156b30c00049a4097',
+      date: '2020-05-04T13:00:46.466Z',
+      clack: 149,
+      mult: 756,
+      wash: 473
+    },
+    {
+      _id: '5eb16a20b6dff44855a64793',
+      date: '2020-05-05T13:24:14.318Z',
+      clack: 146,
+      mult: 737,
+      wash: 459
+    },
+    {
+      _id: '5eb2b6071edf1300044d5a32',
+      date: '2020-05-06T13:00:20.142Z',
+      clack: 147,
+      mult: 718,
+      wash: 451
+    },
+    {
+      _id: '5eb42d91b36d9bac1c6a68ff',
+      date: '2020-05-07T15:42:48.966Z',
+      clack: 144,
+      mult: 732,
+      wash: 456
+    },
+    {
+      _id: '5eb576b59123d9bbe64fa04c',
+      date: '2020-05-08T15:07:08.511Z',
+      clack: 142,
+      mult: 720,
+      wash: 452
+    },
+    {
+      _id: '5eb6aa787e0d0400044d5e19',
+      date: '2020-05-09T13:00:15.368Z',
+      clack: 143,
+      mult: 701,
+      wash: 461
+    },
+    {
+      _id: '5eb7fc024438610004ef0801',
+      date: '2020-05-10T13:00:15.991Z',
+      clack: 149,
+      mult: 725,
+      wash: 467
+    },
+    {
+      _id: '5eb94d8903abdd000421f39e',
+      date: '2020-05-11T13:00:22.798Z',
+      clack: 148,
+      mult: 735,
+      wash: 469
+    },
+    {
+      _id: '5ebab7a0d06a555f5300f3cc',
+      date: '2020-05-12T14:45:17.658Z',
+      clack: 149,
+      mult: 717,
+      wash: 461
+    },
+    {
+      _id: '5ebbf08907eff50004be4e8c',
+      date: '2020-05-13T13:00:32.208Z',
+      clack: 142,
+      mult: 692,
+      wash: 451
+    },
+    {
+      _id: '5ebd4233ddbfc40004eaad35',
+      date: '2020-05-14T13:01:13.889Z',
+      clack: 145,
+      mult: 688,
+      wash: 464
+    },
+    {
+      _id: '5ebebda2c33780d2d702b451',
+      date: '2020-05-15T16:00:08.615Z',
+      clack: 142,
+      mult: 687,
+      wash: 454
+    },
+    {
+      _id: '5ebfe52a59d8e200046efe58',
+      date: '2020-05-16T13:00:54.584Z',
+      clack: 147,
+      mult: 702,
+      wash: 462
+    },
+    {
+      _id: '5ec136be0c95290004dffc51',
+      date: '2020-05-17T13:01:05.067Z',
+      clack: 151,
+      mult: 733,
+      wash: 469
+    },
+    {
+      _id: '5ec28818463c22000444d2ba',
+      date: '2020-05-18T13:00:26.417Z',
+      clack: 153,
+      mult: 751,
+      wash: 472
+    },
+    {
+      _id: '5ec3e02485b5ba421df2a7d8',
+      date: '2020-05-19T13:28:34.067Z',
+      clack: 148,
+      mult: 720,
+      wash: 469
+    },
+    {
+      _id: '5ec52b34df162d000477f350',
+      date: '2020-05-20T13:00:54.965Z',
+      clack: 151,
+      mult: 711,
+      wash: 462
+    },
+    {
+      _id: '5ec67c989622c10004f0cd3b',
+      date: '2020-05-21T13:00:17.046Z',
+      clack: 156,
+      mult: 712,
+      wash: 467
+    },
+    {
+      _id: '5ec7e84c2659b249f57d686f',
+      date: '2020-05-22T14:52:15.863Z',
+      clack: 155,
+      mult: 704,
+      wash: 467
+    },
+    {
+      _id: '5ec91fa278966500043b3d1d',
+      date: '2020-05-23T13:00:27.260Z',
+      clack: 158,
+      mult: 707,
+      wash: 479
+    },
+    {
+      _id: '5eca7117198bfe0004fca649',
+      date: '2020-05-24T13:00:16.069Z',
+      clack: 160,
+      mult: 737,
+      wash: 483
+    },
+    {
+      _id: '5ecbc2a8474db30004f825fa',
+      date: '2020-05-25T13:00:23.012Z',
+      clack: 164,
+      mult: 743,
+      wash: 492
+    },
+    {
+      _id: '5ecd17f73d0c800004b01200',
+      date: '2020-05-26T13:00:16.949Z',
+      clack: 163,
+      mult: 759,
+      wash: 511
+    },
+    {
+      _id: '5ece65d5120e7a00046ec325',
+      date: '2020-05-27T13:01:08.022Z',
+      clack: 165,
+      mult: 740,
+      wash: 505
+    }
+  ] };
