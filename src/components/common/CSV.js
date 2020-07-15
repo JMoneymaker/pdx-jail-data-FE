4c8332548460720290c3d58a02dfaae14cf685cc
AaronMullan <aaroncmullan@gmail.com>
2020-07-15 13:59:46 -0700

    download image in button and hover on button working

--- src/components/common/CSV.js
+++ src/components/common/CSV.js
@@ -3,19 +3,45 @@ import { CSVLink } from 'react-csv';
 import PropTypes from 'prop-types';
 import styles from '../chart-scroll/ChartScroll.css';
 import downloadImage from '../../assets/download6.png';
+import Modal from 'react-modal';
 
 const CSV = ({ data, filename }) => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal(){
+    setIsOpen(false);
+  }
   return (
-    <div className={styles.csv}>
-      <CSVLink 
-        data={[...data]}
-        filename={filename}
-        target='_blank'>
-        <p className={styles.downloadContainer}>
-          <img className={styles.downloadImage} src={downloadImage} alt='download' />
-          <span className={styles.toolTip}>Download a CSV of this data</span>
-        </p>
-      </CSVLink>
+    <div>
+      {/* <img className={styles.downloadImage} src={downloadImage} alt='download' /> */}
+      <div >
+        <button 
+          onClick={openModal}
+          className={styles.downloadHider}>
+          <img 
+            className={styles.downloadImage}
+            src={downloadImage}
+          />
+          <p className={styles.downloadContainer}>
+            <span className={styles.toolTip}>Download a CSV of this data</span>
+          </p>
+        </button>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+      >
+        <div className={styles.csv}>
+          <CSVLink 
+            data={[...data]}
+            filename={filename}
+            target='_blank'>
+            download
+          </CSVLink>
+        </div>
+        <button onClick={closeModal}>close</button>
+      </Modal>
     </div>
   );
 };
