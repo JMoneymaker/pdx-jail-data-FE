44d2c568a965b2acc6e696834a1c09426c57c189
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-08-08 23:17:46 -0700

    https

--- src/services/getDailyCounts.js
+++ src/services/getDailyCounts.js
@@ -1,44 +1,44 @@
 export const getDailyAgeCount = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyAgeCount`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyAgeCount`)
     .then(res => res.json());
 };
 
 export const getDailyAgencyCount = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyAgencyCount`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyAgencyCount`)
     .then(res => res.json());
 };
 
 export const getDailyFacilityCount = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyFacilityCount`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyFacilityCount`)
     .then(res => res.json());
 };
 
 export const getDailyGenderCount = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyGenderCount`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyGenderCount`)
     .then(res => res.json());
 };
 
 export const getDailyRaceCount = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyRaceCount`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyRaceCount`)
     .then(res => res.json());
 };
 
 export const getDailyCount = (county, category) => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/daily${category}Count`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/daily${category}Count`)
     .then(res => res.json());
 };
 
 export const getDailyChargeSeverityCount = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyChargeCategory`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyChargeCategory`)
     .then(res => res.json());
 };
 
 export const getDailyChargeDescriptions = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/dailyChargeDescriptions`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/dailyChargeDescriptions`)
     .then(res => res.json());
 };
 
 export const getDailyTopCharges = county => {
-  return fetch(`http://157.245.170.56/api/v1/${county}/top20charges`)
+  return fetch(`https://157.245.170.56/api/v1/${county}/top20charges`)
     .then(res => res.json());
 };
