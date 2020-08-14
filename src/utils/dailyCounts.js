515b9c91831c8d2b287b34b94304d6746d5af299
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-08-14 13:32:04 -0700

    sort charge severity

--- src/utils/dailyCounts.js
+++ src/utils/dailyCounts.js
@@ -264,8 +264,22 @@ export const shapeAge = res => {
     });
 };
 
+
+const sortSeverity = res => {
+  let ordering = {}, // map for efficient lookup of sortIndex
+    sortOrder = ['A Felony', 'B Felony', 'C Felony', 'U Felony', 'A Misdemeanor', 'B Misdemeanor', 'C Misdemeanor', 'U Misdemeanor', 'U Unknown'];
+  for(let i = 0; i < sortOrder.length; i++)
+    ordering[sortOrder[i]] = i;
+  res.sort(function(a, b) {
+    return (ordering[b._id] - ordering[a._id]);
+  });
+  return res;
+};
+
+
 export const shapeChargeSeverity = res => {
-  return res.map(item => {
+  const sortedArray = sortSeverity(res);
+  return sortedArray.map(item => {
     return ({
       x: item._id,
       y: item.total
